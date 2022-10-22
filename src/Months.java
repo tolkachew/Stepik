8dd1ecca06882a24998807e0952aa100f9fed14c
Vladislav <tolkachew.wlad@yandex.ru>
2022-10-22 16:02:26 +0300

    add tsk

---
+++ src/Months.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Example {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        switch (day) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("error");
+        }
+    }
+}
